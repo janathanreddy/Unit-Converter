eaaad6fa356d752d3948fbc4d0299068a87b07c3
janarthan s <janathanreddy@gmail.com>
2021-01-28 21:25:19 +0530

    Speed Converter Feet,Kilometer,Mile Covered Done

--- UnitConvert/SpeedViewController.swift
+++ UnitConvert/SpeedViewController.swift
@@ -7,23 +7,106 @@
 
 import UIKit
 
-class SpeedViewController: UIViewController {
+class SpeedViewController: UIViewController,UITextFieldDelegate {
+
+    
+    @IBOutlet weak var MetrePerSec: UITextField!
+    
+    @IBOutlet weak var FeetperMin: UITextField!
+    
+    @IBOutlet weak var KilometrePerHour: UITextField!
+    
+    @IBOutlet weak var MilePerHour: UITextField!
+    let speedConverter = SpeedConverter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        MetrePerSec.delegate = self
+        FeetperMin.delegate = self
+        KilometrePerHour.delegate = self
+        MilePerHour.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func MeterChange(_ sender: UITextField) {
+        let value = sender.text!
+        
+        if(!value.isEmpty){
+            
+            let metreSec = Double(value)
+            let values = speedConverter.metreSecConverter(metre: metreSec!)
+            
+            FeetperMin.text = String(values.feet)
+            KilometrePerHour.text = String(values.km)
+            MilePerHour.text = String(values.mile)
+            
+        }else{
+            resetFields()
+        }
     }
-    */
-
+    
+    @IBAction func FeetChange(_ sender: UITextField) {
+        let value = sender.text!
+        
+        if(!value.isEmpty){
+            
+            let feetMin = Double(value)
+            let values = speedConverter.feetMinConverter(feet: feetMin!)
+            
+            MetrePerSec.text = String(values.metre)
+            KilometrePerHour.text = String(values.km)
+            MilePerHour.text = String(values.mile)
+            
+        }else{
+            resetFields()
+        }
+    }
+    
+    @IBAction func KilometerChange(_ sender: UITextField) {
+        let value = sender.text!
+        
+        if(!value.isEmpty){
+            
+            let kmHour = Double(value)
+            let values = speedConverter.kmHourConverter(km: kmHour!)
+            
+            MetrePerSec.text = String(values.metre)
+            FeetperMin.text = String(values.feet)
+            MilePerHour.text = String(values.mile)
+            
+        }else{
+            resetFields()
+        }
+    }
+    
+    @IBAction func MileChange(_ sender: UITextField) {
+        let value = sender.text!
+        
+        if(!value.isEmpty){
+            
+            let mileHour = Double(value)
+            let values = speedConverter.mileHourConverter(mile: mileHour!)
+            
+            MetrePerSec.text = String(values.metre)
+            FeetperMin.text = String(values.feet)
+            KilometrePerHour.text = String(values.km)
+            
+        }else{
+            resetFields()
+        }
+    }
+    func resetFields() -> () {
+        MetrePerSec.text = ""
+        FeetperMin.text = ""
+        KilometrePerHour.text = ""
+        MilePerHour.text = ""
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if string.isEmpty { return true }
+        
+        let currentText = textField.text ?? ""
+        let replacementText = (currentText as NSString).replacingCharacters(in: range, with: string)
+        
+        return replacementText.isValidDouble(maxDecimalPlaces: 4)
+    }
 }
